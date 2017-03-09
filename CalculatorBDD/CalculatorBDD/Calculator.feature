0fbd19192977962a19bf8f0549bd41490e366674
jrdpeers <jim.peers@gmail.com>
2017-03-09 15:25:40 -0800

    Initial commit

---
+++ CalculatorBDD/CalculatorBDD/Calculator.feature
@@ -0,0 +1,17 @@
+﻿Feature: Calculator
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have also entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+Scenario: Multiply two numbers
+	Given I have entered 3 into the calculator
+	And I have also entered 5 into the calculator
+	When I press multiply
+	Then the result should be 15 on the screen
---
+++ CalculatorBDD/CalculatorBDD/CalculatorSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Example;
+using NUnit.Framework;
+
+namespace CalculatorBDD
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private Calculator calculator = new Calculator();
+        private int result { get; set; }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+        
+        [When(@"I press multiply")]
+        public void WhenIPressMultiply()
+        {
+            result = calculator.Multiply();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
